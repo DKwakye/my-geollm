52c2f4c711c4ea83f461c61ed610edc01aba7d89
daniels <admin@admins-MacBook-Air.local>
2023-11-07 15:03:49 +0100

    first geollm training trial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Finetune an LLM for Downstream geospatial applications',
+    author='Daniel Kwakye',
+    license='MIT',
+)
